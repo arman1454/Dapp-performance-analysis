73b5a2fd5a50974453d217ce177c1d4d899c1db2
Arman <fakidarman@gmail.com>
2024-09-16 22:52:27 +0600

    added hardward resource usage to both Ethereum and Tron

--- EthereumBalance.js
+++ EthereumBalance.js
@@ -6,7 +6,7 @@
 const { ethers } = require('ethers');
 require('dotenv').config();
 // const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_CONNECT);
-const provider = new ethers.providers.JsonRpcProvider(process.env.Holesky_Connect);
+const provider = new ethers.providers.JsonRpcProvider(process.env.Sepolia_Connect);
 
 const WalletAddress = '0x48B5B6435f773e5a4Dc02de1d7f5850ec7f3e7B7';
 // Private Key: 0xec72f1d9f20d8056902d301da1ff8d08029adf05420bf97c485162e9c3244ccd
