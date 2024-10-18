e1d4d4f28466c98e7299449fbd3df28f6cec620b
Arman <fakidarman@gmail.com>
2024-10-19 00:14:03 +0600

    deleted unnecessary file

--- GenerateEthereumWallet.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require('ethers');
-
-// Generate a random wallet
-const wallet = ethers.Wallet.createRandom();
-
-console.log('Address:', wallet.address);
-console.log('Private Key:', wallet.privateKey);
